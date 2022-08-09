21f90bbea88346383be20a1caf31d66f37e66ca5
shahid <shahdbest@gmail.com>
2022-08-09 12:42:47 +0530

    assignemnt

---
+++ src/main/java/com/stackroute/basics/SortAscNumber.java
@@ -0,0 +1,20 @@
+package com.stackroute.basics;
+
+
+public class SortAscNumber {
+
+    public static void main(String[] args) {
+        new SortAscNumber().getNumbers();
+    }
+
+    //get the numbers from user through console
+    public void getNumbers() {
+
+    }
+
+    //logic to sort the numbers
+    public String numberSorter(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) {
+
+        return null;
+    }
+}
